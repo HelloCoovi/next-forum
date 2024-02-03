40c8ec9798e4b34da9d44bff3d664719844eb3c5
jangg0627 <jangg0627@naver.com>
2024-02-03 16:15:16 +0900

    🎯🗃️📦 MongoDB 연결 - yarn add mongodb - 인터넷에 있는 연결 코드를 활용해서 BD연결 및 데이터 불러오기(database.js)   -> 개발중 저장할떄 마다 DB에 접속하는것을 막기 위한 예외처리 존재

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import { connectDB } from "@/utils/database.js";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const db = (await connectDB).db('forum');
+  let result = await db.collection('post').find().toArray();
+
+  // console.log(result)
+
   return (
     <h2>초기세팅</h2>
   );
---
+++ src/utils/database.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb'
+
+const url = process.env.REACT_APP_MONGODB_URI;
+const options = { useNewUrlParser: true }
+let connectDB
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect()
+  }
+  connectDB = global._mongo
+} else {
+  connectDB = new MongoClient(url, options).connect()
+}
+export { connectDB }
+
+// import { connectDB } from "@/utils/database.js";
+// const db = (await connectDB).db('forum');
+// let result = await db.collection('post').find().toArray();
