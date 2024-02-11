79e0d49a9050a3cb03ff7c32c2cbd96c2ece8cca
HelloCoovi <hellocoovi@gmail.com>
2024-02-11 23:28:20 +0900

    🎯🎯🗃️ 삭제기능 구현 + 컴포넌트 분리 - 클릭시 데이터를 삭제하는 버튼 구현 - client 컴포넌트를 따로 분리해 server 컴포넌트에서 데이터를 props로 받게 설정   -> 보안, SEO 향상

---
+++ src/app/list/ListItem.js
@@ -0,0 +1,42 @@
+'use client'
+
+import Link from "next/link.js"
+import DetailLink from "./DetailLink.js";
+
+export default function ListItem({ result }) {
+  return (
+    <div>
+      {
+        result.map((data, idx) => {
+          return (
+            <div className="list-item" key={idx}>
+              <Link href={`/detail/${data._id}`}>
+                <h4>{data.title}</h4>
+              </Link>
+              <button onClick={() => {
+                fetch('/api/post/delete', {
+                  method: 'DELETE', // GET, POST, PUT
+                  body: data._id // JSON.stringify(['1', '2']) // 배열, 객체등을 body로 보낼때는 JSON.stringify가 필요
+                })
+                  .then((res) => {
+                    if (res.status == 200) return res.json()
+                    else {
+                      // 서버가 에러코드전송시 실행할코드
+                    }
+                  })
+                  .then((result) => {
+                    // 추가로 성공시 실행할코드
+                  }).catch((error) => {
+                    // 인터넷문제 등으로 실패시 실행할코드
+                    console.log(error)
+                  })
+              }}>🗑️</button>
+              <p>1월 1일</p>
+              <DetailLink postID={data._id.toString()} />
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,27 +1,16 @@
 import { connectDB } from "@/utils/database.js";
 import Link from "next/link.js";
-import DetailLink from "./DetailLink.js";
+
+import ListItem from "./ListItem.js";
 
 export default async function List() {
   const db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
-  console.log(result)
+  result = result.map(doc => ({ ...doc, _id: doc._id.toString() }));
 
   return (
     <div className="list-bg">
-      {
-        result.map((data, idx) => {
-          return (
-            <div className="list-item" key={idx}>
-              <Link href={`/detail/${data._id}`}>
-                <h4>{data.title}</h4>
-              </Link>
-                <p>1월 1일</p>
-              <DetailLink postID={data._id.toString()} />
-            </div>
-          )
-        })
-      }
+      <ListItem result={result} />
     </div>
   )
 } 
