cd24400731920a31e827206c78ade5e3562a7141
HelloCoovi <hellocoovi@gmail.com>
2024-02-13 00:40:38 +0900

    🎯⚡️ 다양한 데이터 캐싱 방법 정리 - `fetch`는 기본적으로 캐싱을 지원함 - 캐싱 기능을 사용하고 싶다면   -> API 호출 코드를 따로 분리해서 fetch로 호출   -> NEXT의 예약어 `revalidate`등으로 페이지 단위로 캐싱 설정

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,18 @@ import { connectDB } from "@/utils/database.js";
 import Image from "next/image";
 import styles from "./page.module.css";
 
+// 페이지 단위 캐싱(60초 단위로 데이터 갱신)
+export const revalidate = 60
+
 export default async function Home() {
   const db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
-
   // console.log(result)
+
+  // await fetch('/URL', { cache: 'force-cache' }) // 사실 fetch에 기본으로 force-cache 옵션이 있어서 안건드려도 됨
+  // await fetch('/URL', { cache: 'no-store' })
+  // await fetch('/URL', { next: { revalidate: 60 } })
+
 
   return (
     <h2>초기세팅</h2>
