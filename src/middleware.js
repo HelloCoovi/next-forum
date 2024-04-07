30d6907735bd505186fa20397d75eadf79c9ca02
HelloCoovi <hellocoovi@gmail.com>
2024-04-07 23:05:26 +0900

    🎯 list Url에 접근하는 유저 정보 출력하기(log남기기) - startsWith로 /list에 접근하면 로직들이 실행됨 - 현재 시간, 유저의 OS 출력   -> 응용해서 저장하는것도 가능 - 유저 정보 출력 후 요청된 페이지로 이동   -> `NextResponse.next()`

--- src/middleware.js
+++ src/middleware.js
@@ -2,12 +2,9 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
 
-  // middleware 에서는 여러가지 정보에 접근가능
-  console.log(request.nextUrl)
-  console.log(request.cookies)
-  console.log(request.headers)
-
-  NextResponse.next() // 이상없음 그대로 진행
-  NextResponse.redirect() // 다른 페이지로 강제 이동
-  NextResponse.rewrite() // 다른 페이지로 강제 이동(but URL 변경 없음)
-} 
+  if (request.nextUrl.pathname.startsWith('/list')) {
+    console.log(new Date())
+    console.log(request.headers.get('sec-ch-ua-platform')) // OS 출력하기(사파리는 출력안됨)
+    return NextResponse.next()
+  }
+}
