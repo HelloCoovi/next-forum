d4475d852df4a2087e2ece6073d63d79ff993f73
HelloCoovi <hellocoovi@gmail.com>
2024-04-07 17:51:09 +0900

    🎯💩 `localStorage`로 테마 설정하기 - Next.js에서 `localStorage`를 쓰려면 client Side에서만 사용가능   -> server에서는 `localStorage`가 없기때문 - Next에서는 클라이언트 사이드도 서버에서 랜더링을 최대한 하려하기 때문에 그냥 `localStorage`를 사용하면 에러가 날수 있음   -> `useEffect`로 `localStorage` 호출해야함 - 💩 다만 useEffect로 실행시 컴포넌트가 랜더링 된 후 실행되기 때문에 깜빡이는 등의 현상이 생김   -> cookie로 개선 가능

--- src/components/LoginBtn.js
+++ src/components/LoginBtn.js
@@ -1,8 +1,16 @@
 'use client'
 
 import { signIn } from 'next-auth/react'
+import { useEffect } from 'react'
 
 export default function LoginBtn() {
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // localStorage.setItem('테마모드', 'dark')
+      localStorage.getItem('테마모드')
+    }
+  }, [])
 
   return <button onClick={() => signIn()}>로그인</button>
 }
