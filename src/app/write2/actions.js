b15b1b9621588e11c722a642fff2fb4dac4ad8d3
HelloCoovi <hellocoovi@gmail.com>
2024-04-09 01:34:37 +0900

    🎯 next.js의 cache 갱신을 사용한 데이터 불러오기 - 일반적으로 데이터를 보내고 갱신하기위해서 다른 로직이 필요하지만 next의 cache 함수를 사용하면 편하게 처리가능

--- src/app/write2/actions.js
+++ src/app/write2/actions.js
@@ -1,8 +1,14 @@
 'use server'
 
+import { revalidatePath, revalidateTag } from 'next/cache.js'
+// 작업후 데이터를 다시 불러오는 함수 설정
+// revalidatePath: 해당 경로의 데이터를 다시 불러옴
+// revalidateTag: 해당 캐쉬 태그의 데이터를 다시 불러옴
+
 import { connectDB } from "@/utils/database.js"
 
 export async function handleSubmit(formData) {
   const db = (await connectDB).db('forum')
   await db.collection('post_test').insertOne({ title: formData.get('title') })
+  revalidatePath('/write2')
 }
--- src/app/write2/page.js
+++ src/app/write2/page.js
@@ -2,11 +2,18 @@ import { connectDB } from '@/utils/database.js'
 import { handleSubmit } from './actions.js'
 
 export default async function Write2() {
+  const db = (await connectDB).db('forum')
+  let result = await db.collection('post_test').find().toArray()
 
   return (
-    <form action={handleSubmit}>
-      <input type='text' name='title' />
-      <button type='submit'>Submit</button>
-    </form>
+    <div>
+      <form action={handleSubmit}>
+        <input type='text' name='title' />
+        <button type='submit'>Submit</button>
+      </form>
+      {
+        result ? result.map((post, idx) => <p key={post.title}>글제목: {post.title}</p>) : null
+      }
+    </div>
   )
 } 
